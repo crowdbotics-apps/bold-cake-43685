ab050bf14601e6e0afe0f3aeae2c675364ef9c11
Crowdbotics <team@crowdbotics.com>
2023-10-01 02:36:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-cake-43685",
+  url: "https://bold-cake-43685.botics.co",
+  api: "https://bold-cake-43685.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
